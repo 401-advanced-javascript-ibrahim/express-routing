08933542f9ee94ae8e87fba4af4cdf7cde9f3825
ibraim-Ajarmeh <ajarmeh1997@gmail.com>
2020-01-29 18:27:23 +0200

    completed the four CRUD operations

--- routes/categories.js
+++ routes/categories.js
@@ -8,9 +8,10 @@ const router = express.Router();
 router.get('/categories', getCategories)
 router.get('/categories/:id', getOneCategory)
 router.post('/categories', createCategories)
-// router.update('/categories/:id', updateCategories)
-// router.delete('/categories/:id', deleteCategories)
+router.put('/categories/:id', updateCategories)
+router.delete('/categories/:id', deleteCategories)
 
+//// Routes Functions
 function getCategories(req, res, next) {
     console.log(req.params);
     category.get()
@@ -21,15 +22,37 @@ function getCategories(req, res, next) {
 }
 
 function getOneCategory(req, res, next) {
-    category.get(req)
+    category.get(req.params.id)
+        .then(data => {
+            res.status(200).json(data);
+        })
+        .catch(next)
 }
 
 function createCategories(req, res, next) {
+    console.log('before create', req.body);
     category.create(req.body)
         .then(data => {
-            req.params = data._id
+            console.log('after create', req.body);
             res.status(201).json(data)
         })
+        .catch(next)
+}
+
+function updateCategories(req, res, next) {
+    category.update(req.params.id, req.body)
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(next)
+}
+
+function deleteCategories(req, res, next) {
+    category.delete(req.params.id)
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(next)
 }
 
 module.exports = router 
--- routes/products.js
+++ routes/products.js
@@ -6,11 +6,12 @@ const router = express.Router();
 
 //////// Main Routes 
 router.get('/products', getProducts)
-// router.get('/products/:id', getOneProduct)
+router.get('/products/:id', getOneProduct)
 router.post('/products', createProducts)
-// router.update('/products/:id', updateProducts)
-// router.delete('/products/:id', deleteProducts)
+router.put('/products/:id', updateProducts)
+router.delete('/products/:id', deleteProducts)
 
+//// Routes Functions
 function getProducts(req, res, next) {
     product.get()
         .then(data => {
@@ -19,10 +20,34 @@ function getProducts(req, res, next) {
         .catch(next)
 }
 
+function getOneProduct(req, res, next) {
+    product.get(req.params.id)
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(next)
+}
+
 function createProducts(req, res, next) {
     product.create(req.body)
         .then(data => {
             res.status(201).json(data)
+        })
+        .catch(next)
+}
+
+function updateProducts(req, res, next) {
+    product.update(req.params.id, req.body)
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(next)
+}
+
+function deleteProducts(req, res, next) {
+    product.delete(req.params.id)
+        .then(data => {
+            res.status(200).json(data)
         })
 }
 
